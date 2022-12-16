274b7bc17f3ab8dfb09d5aa20cb44905ad2d067a
yermilov.m <yermilov.m@hotmail.com>
2022-12-16 02:04:54 +0100

    we tried ssh

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("edit this");
+        System.out.println("we'll try ssh now");
     }
 }
